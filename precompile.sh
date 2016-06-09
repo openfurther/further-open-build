5cafce305017b7cc9dbb28cd3358b00faa112296
Phillip Warner <euvitudo@gmail.com>
2016-06-09 10:42:57 -0600

    Updating precompile with a few more steps
    Turns out when the maven repo is empty, there are some failures due to dependencies between projects
    that must be resolved before the overall build can proceed.

--- precompile.sh
+++ precompile.sh
@@ -4,3 +4,11 @@ echo $FURTHER_ROOT
 cd ../further-open-precompile
 ant -f build.precompile.xml -Dreference.further.dir=$FURTHER_ROOT
 cd -
+pwd=$PWD
+#for p in `echo further-open-core further-open-core/core further-open-core/core/core-api further-open-core/core/core-test further-open-core/ds further-open-osgi`; do
+for p in `echo further-open-core further-open-core/core further-open-core/core/core-api further-open-core/core/core-test further-open-core/ds`; do
+	cd $p
+	echo $p
+	mvn -N clean install
+	cd $pwd
+done
