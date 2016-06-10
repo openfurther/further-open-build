d3dfb9d7b1b1326a281ab33f6d757b91a265d570
Phillip Warner <euvitudo@gmail.com>
2016-06-10 10:23:43 -0600

    added error checking

--- precompile.sh
+++ precompile.sh
@@ -1,20 +1,36 @@
 #!/bin/sh
 FURTHER_ROOT=$PWD/..
 echo $FURTHER_ROOT
+pwd=$PWD
 cd ../further-open-precompile
 ant -f build.precompile.xml -Dreference.further.dir=$FURTHER_ROOT
-cd -
+status=$?
+if [[ $status -ne 0 ]]; then
+	echo
+	echo "There was an error during precompile."
+	echo
+	exit;
+fi
+cd $pwd
 pwd=$PWD
-#for p in `echo further-open-core further-open-core/core further-open-core/core/core-api further-open-core/core/core-test further-open-core/ds further-open-osgi`; do
-for p in `echo further-open-core further-open-core/core further-open-core/core/core-api further-open-core/core/core-test further-open-core/ds`; do
-	cd $p
-	echo $p
+for module in `echo further-open-core further-open-core/core further-open-core/core/core-api further-open-core/core/core-test further-open-core/ds`; do
+	cd $module
+	echo $module
 	mvn -N clean install
+	status=$?
 	cd $pwd
+	if [[ $status -eq 0 ]] ; then
+		echo "Continuing..."
+	else
+		echo
+		echo "There was an error compiling module: $module..."
+		echo
+		exit;
+	fi
 done
 
 # now check if FURTHER_PASSWORD is set
-if [[ "${FURTHER_PASSWORD}" == "" ]] ; then
+if [[ "${FURTHER_PASSWORD}" = "" ]] ; then
 	echo
 	echo "FURTHER_PASSWORD must be set before proceeding"
 else
